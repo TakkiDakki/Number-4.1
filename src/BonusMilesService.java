9a7d9f367f69567d21f1188e1449b5aa20740dad
Yulia Kobeleva <kobelevaulia@gmail.com>
2021-04-01 21:45:08 +0300

    commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,7 @@
 public class BonusMilesService {
+    public long calculate (int registered) {
+        int price = 15050;
+        int cost = price / 20;
+        return cost;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
-        int miles = service.calculate(price);
+        int price = 15_050;
+        int miles = (int) service.calculate(price);
         System.out.println(miles);
     }
 }
